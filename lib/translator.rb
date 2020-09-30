f77c94b0f4da3d821c967478b53b23aa65cc9b5c
John Exarhouleas <exarhouleasjohn@gmail.com>
2020-09-30 19:29:19 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -25,7 +25,7 @@ def get_japanese_emoticon(location, emoticon)
   end
 end
 
-def get_english_meaning
+def get_english_meaning(location, emoticon)
   # code goes here
   all = load_library(location)
   output = ""
